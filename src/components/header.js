a729484874a583b0139426703974fd025dcfc12c
Djobbo Maïga <corehallabusiness@gmail.com>
2020-01-29 10:56:29 +0000

    landing setup

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,16 @@
 import React from "react";
+import ScrollButton from "./scrollButton";
 import "./header.css";
-import BackgroundImage from "../images/header.jpg";
 
 export default function Header() {
   return (
     <header
       id="header"
       className="alt"
-      styles={{ backgroundImage: `url(${BackgroundImage})` }}
     >
       <h1>Lorem Ipsum</h1>
       <h2>Full stack web developper</h2>
+      <ScrollButton />
     </header>
   );
 }
---
+++ src/components/scrollButton.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function ScrollButton() {
+  return (
+    <button className="scrollButton">\/</button>
+  );
+}
